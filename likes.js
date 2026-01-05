d79a47c886966b6fb058e537d249014fdabade2e
Justin Hilton <justinhiltonphotography@gmail.com>
2026-01-05 15:51:04 +0200

    Switch likes to private JSONBin

--- likes.js
+++ likes.js
@@ -1,14 +1,14 @@
 // likes.js
-const BIN_ID = "694d5086ae596e708fb054cd";
-const MASTER_KEY = "$2a$10$FiqRinztCOCKUBEU2aJP8.K6oZqAd7nE3k94Wtb2h0Zc4.kKDprCi";
-const BIN_URL = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
+const BIN_ID = "695bc0d343b1c97be91aa4d2";
+const MASTER_KEY = "$2a$10$z1oJCSDdJekg8mehGj8W..RFX8qFM4p4mNF7su8oSPMsNUFakj1ly";
+const BIN_URL = `https://api.jsonbin.io/v3/b/${695bc0d343b1c97be91aa4d2}`;
 
 // Load likes from JSONBin
 async function loadLikes() {
   try {
-    const res = await fetch(BIN_URL, {
+    const res = await fetch(https://api.jsonbin.io/v3/b/${695bc0d343b1c97be91aa4d2}, {
       headers: {
-        "X-Master-Key": MASTER_KEY
+        "X-Master-Key": $2a$10$z1oJCSDdJekg8mehGj8W..RFX8qFM4p4mNF7su8oSPMsNUFakj1ly
       }
     });
 
@@ -29,7 +29,7 @@ async function saveLikes(likes) {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
-        "X-Master-Key": MASTER_KEY
+        "X-Master-Key": $2a$10$z1oJCSDdJekg8mehGj8W..RFX8qFM4p4mNF7su8oSPMsNUFakj1ly
       },
       body: JSON.stringify({ likes })
     });
